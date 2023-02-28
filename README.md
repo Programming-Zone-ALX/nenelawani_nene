96b893f9ea067e9c448f1f90b9c035a7558b6e91
NeneLawani <nene@lawani.com>
2023-02-28 08:22:55 +0100

    Create readme, git push script, and skeleton of calculator app

---
+++ README.md
@@ -0,0 +1,1 @@
+Nene's Collaboration Repo in ALX SE Programming Zone Group
---
+++ nene_calculator.c
@@ -0,0 +1,9 @@
+/**
+ * This app is a calculator which can perform the basic math functions of
+ * addition, subtraction, multiplication, division, square root, factorial,
+ * and the rounding up/down of a decimal number to 3 decimal places.
+ */
+
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
